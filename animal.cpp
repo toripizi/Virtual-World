74beb317ac2d4877cadd12f1d0a17bb15e4f8a15
toripizi <maciekgoncerzewicz@gmail.com>
2021-04-30 22:27:44 +0200

    square

--- animal.cpp
+++ animal.cpp
@@ -1,7 +1,22 @@
 #include "animal.h"
+#include "plant.h"
 
 void Animal::action() {
+	world.tab[x][y].setSign(' ');
+	world.tab[x][y].organism = nullptr;
 
+	changeXY(xX, yY);
+
+	if (world.tab[x][y].organism == nullptr) {
+		world.tab[x][y].organism = this;
+		world.tab[x][y].setSign(this->getSign());
+	}
+	else if(Animal* animal = dynamic_cast<Animal*>(world.tab[x][y].organism)) {
+		std::cout<<this->name;
+	}
+	else if(Plant* plant = dynamic_cast<Plant*>(world.tab[x][y].organism)){
+
+	}
 }
 void Animal::conflict() {
 
